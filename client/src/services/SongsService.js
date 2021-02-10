6c87baceaa62707c7ee0cf5d031a1da942e23fac
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2021-02-11 01:11:18 +0700

    Styling the ui with Panel.vue and rendering error function

---
+++ client/src/services/SongsService.js
@@ -0,0 +1,16 @@
+import Api from '@/services/Api'
+
+export default {
+  index () {
+    return Api().get('songs')
+  },
+
+  post (song) {
+    return Api().post('songs', song)
+  }
+}
+
+/* AuthenticationService.register({
+  email: 'testing@gmail.com',
+  password: '123456789'
+}) */
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,13 +1,22 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const SongsController = require('./controllers/SongsController')
 
 module.exports = (app) => {
+    /* Authentication */
     app.post('/register',
         AuthenticationControllerPolicy.register,
         AuthenticationController.register
     )
-
     app.post('/login',
         AuthenticationController.login
+    )
+
+    /* Song */
+    app.get('/songs',
+        SongsController.index
+    )
+    app.post('/songs',
+        SongsController.post
     )
 }
